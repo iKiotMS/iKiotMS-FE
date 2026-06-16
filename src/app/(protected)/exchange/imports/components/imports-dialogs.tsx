835ac15c0216cd369b1c85d0e2c4f156dd1553dd
Nguyen Viet Hoang <viethoang3132@gmail.com>
2026-06-16 14:40:06 +0700

    refactor: align stock movement UI with template

--- src/app/(protected)/exchange/imports/components/imports-dialogs.tsx
+++ src/app/(protected)/exchange/imports/components/imports-dialogs.tsx
@@ -1,23 +1,15 @@
-'use client'
+"use client";
 
-import { useImports } from './imports-provider'
-import { ImportsCreateDialog } from './imports-create-dialog'
-import { ImportsDetailSheet } from './imports-detail-sheet'
+import { useImports } from "./imports-provider";
+import { ImportsCreateDialog } from "./imports-create-dialog";
 
 export function ImportsDialogs() {
-  const { open, setOpen, currentRow } = useImports()
+  const { open, setOpen } = useImports();
 
   return (
-    <>
-      <ImportsCreateDialog
-        open={open === 'create'}
-        onOpenChange={(v) => !v && setOpen(null)}
-      />
-      <ImportsDetailSheet
-        open={open === 'detail'}
-        onOpenChange={(v) => !v && setOpen(null)}
-        request={currentRow}
-      />
-    </>
-  )
+    <ImportsCreateDialog
+      open={open === "create"}
+      onOpenChange={(value) => !value && setOpen(null)}
+    />
+  );
 }
